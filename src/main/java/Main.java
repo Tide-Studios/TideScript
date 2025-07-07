550a6c1810b3f7698ba2064887f7404774c40866
Brohammer5 <78236275+Brohammer5@users.noreply.github.com>
2025-07-06 17:12:54 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ public class Main{
 
 
     public static void main(String[] args)  {
-        Config.EncryptionKey = "f9p8LTSn9rASu6ssOdbsOMb2tFRgt5zT";
-
        TideScript tideScript = new TideScript(Config.saveLogs,Config.plugins);
 
 
